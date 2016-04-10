0f7dc12520f19de64c8cf0f6d28506633783f397
Romain Vigo Benia <romain.vigobenia@gmail.com>
2016-04-10 14:06:41 +0200

    Update breadrcumbs formatting not to include link on the last node

--- app/helpers/seo_cms/articles_helper.rb
+++ app/helpers/seo_cms/articles_helper.rb
@@ -1,7 +1,14 @@
 module SeoCms
   module ArticlesHelper
     def generate_breadcrumbs(breadcrumbs_info)
-      breadcrumbs_info.map { |breadcrumb| link_to breadcrumb[:label], breadcrumb[:url] }.join(' > ').html_safe
+      breadcrumbs_info.each_with_index.map do |breadcrumb, i|
+        last_element = (i == breadcrumbs_info.size - 1)
+        if last_element
+          breadcrumb[:label]
+        else
+          link_to(breadcrumb[:label], breadcrumb[:url])
+        end
+      end.join(' > ').html_safe
     end
   end
 end
