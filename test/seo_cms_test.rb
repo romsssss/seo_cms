91ff44826501c346f36f147c6c02dc8596c3f0e5
Romain Vigo Benia <romain.vigobenia@gmail.com>
2016-04-11 18:26:37 +0200

    Add unit specs to Article modele

--- test/seo_cms_test.rb
+++ test/seo_cms_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class SeoCmsTest < ActiveSupport::TestCase
-  test "truth" do
+  test 'is a module' do
     assert_kind_of Module, SeoCms
   end
 end
--- test/unit/seo_cms/article_test.rb
+++ test/unit/seo_cms/article_test.rb
@@ -2,8 +2,54 @@ require 'test_helper'
 
 module SeoCms
   class ArticleTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+
+    setup do
+      @ninja_turtles = seo_cms_articles(:ninja_turtles)
+      @michaelangelo = seo_cms_articles(:michaelangelo)
+      @michaelangelo.parent = @ninja_turtles
+    end
+
+    test "should not save without uri" do
+      article = Article.new
+      assert(!article.valid?)
+    end
+
+    test "should have an unique uri" do
+      article1 = Article.create(uri: 'my_uri')
+      assert(article1.valid?)
+      article2 = Article.new(uri: 'my_uri')
+      assert(!article2.valid?)
+    end
+
+    test "has ancestry" do
+      assert_respond_to(@ninja_turtles, :parent)
+    end
+
+    # url
+    test "url returns the right url (without mounted path)" do
+      assert_equal(@michaelangelo.url(false), '/ninja-turtles/michaelangelo')
+    end
+    test "url returns the right url (with mounted path)" do
+      assert_equal(@michaelangelo.url, '/seo_cms/ninja-turtles/michaelangelo')
+    end
+
+    # breadcrumb_info
+    test "breadcrumb_info returns a Hash" do
+      assert_kind_of(Hash, @ninja_turtles.breadcrumb_info)
+    end
+    test "breadcrumb_info returns label and url information" do
+      breadcrumb_info = @ninja_turtles.breadcrumb_info
+      assert_equal(breadcrumb_info[:label], 'Ninja Turtles')
+      assert_equal(breadcrumb_info[:url], '/seo_cms/ninja-turtles')
+    end
+
+    # breadcrumbs_info
+    test "breadcrumbs_info returns an array" do
+      assert_kind_of(Array, @ninja_turtles.breadcrumbs_info)
+    end
+    test "breadcrumbs_info returns its parents info" do
+      assert_equal(@michaelangelo.breadcrumbs_info.size, 2)
+    end
+
   end
 end
