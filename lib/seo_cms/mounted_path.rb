3e0af216a7872759a53b4423245f4f3b3808cdd0
Romain Vigo Benia <romain.vigobenia@gmail.com>
2016-04-11 18:50:42 +0200

    Move some code logic into lib/ folder

---
+++ lib/seo_cms/mounted_path.rb
@@ -0,0 +1,8 @@
+class Rails::Engine
+  def self.mounted_path
+    route = Rails.application.routes.routes.detect do |route|
+      route.app == self
+    end
+    route && route.path
+  end
+end
--- test/seo_cms_test.rb
+++ test/seo_cms_test.rb
@@ -4,4 +4,9 @@ class SeoCmsTest < ActiveSupport::TestCase
   test 'is a module' do
     assert_kind_of Module, SeoCms
   end
+
+  test 'can get the path where it is mounted' do
+    assert_respond_to(SeoCms::Engine, :mounted_path)
+    assert_kind_of(Journey::Path::Pattern, SeoCms::Engine.mounted_path)
+  end
 end
