4a766a80b6f5da16db7eddfc4a3a30b29748c7be
Romain Vigo Benia <romain.vigobenia@gmail.com>
2016-04-12 10:50:15 +0200

    Add routes tests

---
+++ test/routes_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class RoutesTest < ActionController::TestCase
+
+  setup do
+    @article = seo_cms_articles(:michaelangelo)
+    @routes = SeoCms::Engine.routes
+  end
+
+  test 'content management is reachable with /seo_content' do
+    assert_routing(
+      { method: 'get', path: '/seo_content' },
+      { controller: 'seo_cms/articles', action: 'index' }
+    )
+  end
+
+  test 'Articles routes are auto generated' do
+    assert_routing(
+      { method: 'get', path: @article.url(false) },
+      { controller: 'seo_cms/articles', action: 'show', id: @article.id }
+    )
+  end
+
+end
